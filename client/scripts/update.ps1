8da36c4e69cbf7069d6432726dad10a9bb005d07
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-06-25 00:34:10 +0300

    Client

---
+++ client/scripts/update.ps1
@@ -0,0 +1,3 @@
+cd ./client/Lykke.Service.CandlesHistory.Client/
+iwr http://localhost:5000/swagger/v1/swagger.json -o Service.CandlesHistory.json
+autorest --input-file=Service.CandlesHistory.json --csharp --namespace=Lykke.Service.CandlesHistory.Client --output-folder=./
