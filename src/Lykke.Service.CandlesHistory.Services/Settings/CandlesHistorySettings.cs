99e67cc8e335ac33ed547d1afc4c65d318b889d1
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-06-22 19:37:42 +0300

    LWDEV-8046 Removed an unused parameter from settings.

--- src/Lykke.Service.CandlesHistory.Services/Settings/CandlesHistorySettings.cs
+++ src/Lykke.Service.CandlesHistory.Services/Settings/CandlesHistorySettings.cs
@@ -7,7 +7,6 @@ namespace Lykke.Service.CandlesHistory.Services.Settings
     {        
         public AssetsCacheSettings AssetsCache { get; set; }
         public DbSettings Db { get; set; }
-        public int MaxCandlesCountWhichCanBeRequested { get; set; }
         public ResourceMonitorSettings ResourceMonitor { get; set; }
     }
 }
