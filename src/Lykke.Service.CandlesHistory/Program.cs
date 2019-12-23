2f834e0f88a5c85e0bea320971c3aa11bfb3f8f2
Aliaksandr Vashetsin <vashetsin@gmail.com>
2019-12-23 19:34:11 +0100

    LT-1919: Lykke.Service.CandlesHistory: add WebHostLogger

--- src/Lykke.Service.CandlesHistory/Program.cs
+++ src/Lykke.Service.CandlesHistory/Program.cs
@@ -18,6 +18,8 @@ namespace Lykke.Service.CandlesHistory
     [UsedImplicitly]
     internal sealed class Program
     {
+        internal static IWebHost Host { get; private set; }
+
         public static string EnvInfo => Environment.GetEnvironmentVariable("ENV_INFO");
         
         public static async Task Main(string[] args)
@@ -42,14 +44,14 @@ namespace Lykke.Service.CandlesHistory
                         .AddUserSecrets<Startup>()
                         .AddEnvironmentVariables()
                         .Build();
-                    
-                    var host = WebHost.CreateDefaultBuilder()
+
+                    Host = WebHost.CreateDefaultBuilder()
                         .UseConfiguration(configuration)
                         .UseStartup<Startup>()
                         .UseApplicationInsights()
                         .Build();
 
-                    await host.RunAsync();
+                    await Host.RunAsync();
                 }
                 catch (Exception e)
                 {
